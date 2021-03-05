4f52408df69e80d478fc6385a2725fdaf027e30c
diana <diana@DESKTOP-AE9RS0A>
2021-03-05 18:13:07 +0300

    1

--- Date.cs
+++ Date.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    class Date
+    class Date : Triad
     {
+
         //Вычисление даты через заданное количество дней
         //Вычитание заданного количества дней из даты
         //Определение високосности года
         //Присвоение и получение отдельных частей(год, месяц, день)
         //Сравнение дат(равно, до, после)
         //Вычисление количества дней между датами
+
+
+        public Date(int year, int month, int day) : base(year, month, day)
+        {
+        }
+
+        public Date(string st) : base(st)
+        {
+        }
+
+        public Date(DateTime dt) : base(dt)
+        {
+        }
+
+        public override int inc()
+        {
+            return day++;
+        }
     }
 }
